02953718c3ef279354295796a836fe4641bb7514
Mimsseey <56138496+Mimsseey@users.noreply.github.com>
2019-11-27 08:59:05 +0100

    Update time.cpp

--- time.cpp
+++ time.cpp
@@ -1,7 +1,7 @@
 #include "time.h"
 
 unsigned int localPort = 2390;      // local port to listen for UDP packets
-IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server IP Adrress find nearest NTP via pool.ntp.org
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
